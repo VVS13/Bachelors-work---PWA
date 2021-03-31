9eeee4f006d2488229291e5b2b4c65648c8b897b
VVS13 <sivakovsvladislavs@gmail.com>
2021-04-01 00:00:53 +0300

    added react

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>
+        lkjfdglksdbfglkrrrrrrrrrrrskrrrprrrr test
+      </h1>
+    </div>
+  )
+}
+
+export default App
