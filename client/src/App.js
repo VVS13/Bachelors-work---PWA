c757b328ab2116d882e79a3deea4b42ef27a6323
VVS13 <sivakovsvladislavs@gmail.com>
2021-05-18 21:36:36 +0300

    buffering and flashing fixed... next: nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import {React} from 'react'
 import {BrowserRouter as Router} from 'react-router-dom'
 
 import {Switch, Route, Redirect, Link} from 'react-router-dom'
@@ -17,7 +17,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css"
 
 function App() {
 
-  const {token,login,logout,userId} = useAuth()
+  const {token,login,logout,userId,readLoginStatus} = useAuth()
   const isAuthenticated = !!token //
 
   //const routes = useRoutes(isAuthenticated) //changed true/false to be dynamic
@@ -28,45 +28,51 @@ function App() {
   //useEffect is being slow in auth.hook,js.... need another check or usestate to fix it 
   //in that case a loading screen *could* be needed 
 
-  return (
+  if(!readLoginStatus){
+    return (<div>I guess it is loading...</div>)
+  }else{
+    return (
+      <AuthContext.Provider value={{token,userId, login, logout, isAuthenticated}}>
+        <Router> 
+          <Switch>
+  
+            {isAuthenticated? (
+            <>
+            
+            <Route exact path="/">
+              
+              <MainPage />
+              
+            </Route>
+            {/* <Redirect to="/main_page"/> */}
+  
+            <Route path="/create_room">
+              <CreateRoom/>
+            </Route>
+  
+            <Route path="/note_room/:id">
+              <NoteRoom/>
+            </Route>
+            </>
+            ) : ( 
+            <>
+            <Route path="/" >
+              <AuthPage/>
+            </Route>
+            <Redirect to="/" />
+            </>
+            )}
+  
+          </Switch>
+        </Router>
+  
+      </AuthContext.Provider>
+      
+    )
+  }
 
-    <AuthContext.Provider value={{token,userId, login, logout, isAuthenticated}}>
-      <Router> 
 
-      <Switch>
-
-        {isAuthenticated ? (
-        <>
-        <Route exact path="/">
-          <MainPage />
-          {/* <Link to="/main_page"></Link> */}
-        </Route>
-        {/* <Redirect to="/main_page"/> */}
-
-        <Route path="/create_room">
-          <CreateRoom/>
-        </Route>
-
-        <Route path="/note_room/:id">
-           <NoteRoom/>
-        </Route>
-        </>
-        ) : ( 
-        <>
-        <Route path="/">
-          <AuthPage/>
-        </Route>
-        {/* <Redirect to="/" /> */}
-        </>
-        )}
-
-      </Switch>
-
-    </Router>
-
-    </AuthContext.Provider>
-    
-  )
+  
 }
 
 export default App
--- client/src/hooks/auth.hook.js
+++ client/src/hooks/auth.hook.js
@@ -1,6 +1,6 @@
 //need to hold jwt in local storage 
 
-import {useState, useCallback, useEffect} from 'react'  //
+import {useState, useCallback, useEffect,history} from 'react'  //
 
 const storageName = 'userData'
 
@@ -9,27 +9,45 @@ export const useAuth = () => {
     const [token,setToken] = useState(null)
     const [userId,setUserId] = useState(null)
 
+    const [readLoginStatus, setLoginStatus] = useState(false);//
+    const [firstTime, setFirstTime] = useState(false);
+
     const login = useCallback((jwtToken,id) => {  //
         setToken(jwtToken)
         setUserId(id)
         localStorage.setItem(storageName, JSON.stringify({token: jwtToken, userId: id})) //userId, token
-
     },[])
 
     const logout = useCallback(() => {
         setToken(null)
         setUserId(null)
         localStorage.removeItem(storageName)
-    },[])
 
+        setLoginStatus(false)///////////
+        setFirstTime(false)
+
+    },[])
     
     useEffect (() => {
         const data = JSON.parse(localStorage.getItem(storageName))
+
+        setLoginStatus(true)
+        // info that there is something in local storage that passed auth checks
+
         if(data!=null &&data.token!=null){
             login(data.token,data.userId)
         }
     },[login])
 
+    useEffect (() => {
+        if (readLoginStatus && !firstTime) {
+            //history.push("/main_page")
+            setFirstTime(true)
+            //on first 
+
+        }
+    },[readLoginStatus,firstTime])
+
     
-    return{login,logout,userId,token}
+    return{login,logout,userId,token,readLoginStatus}
 }
